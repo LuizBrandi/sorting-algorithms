24f87008ed38f72f8e03b54988fe88c2df279b9a
LuizBrandi <luizfilipebrandi@gmail.com>
2022-05-05 13:30:21 -0300

    creating bubble function

--- bubble.c
+++ bubble.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
-void bubble(){
-    
+void bubble(void * v, int vSize, int typeSize, int (* cmp)(void *, void *)){
+    int i, end, swapped;
+
+    for(end = vSize - 1; end > 0; end--){
+        swapped = 0;
+        for(i = 0; i < end; i++){
+
+            void * b1 = access(v, i, typeSize);
+            void * b2 = access(v, i, typeSize);
+
+            if(cmp(b1, b2)){
+                swap(b1, b2, typeSize);
+                swapped = 1;
+            }
+
+        }
+        if(!swapped) return;
+    }
 }
